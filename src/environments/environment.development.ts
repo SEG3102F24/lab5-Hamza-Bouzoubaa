3a8814c6abda4f9a96b8ffe9fc93011f3ca8c885
Hamza <104928656+Hamza-Bouzoubaa@users.noreply.github.com>
2024-10-20 15:53:54 -0400

    3adi

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase : {
+        apiKey: "AIzaSyBj56RAbrXTBhu8opCN9SC2wa3-P9_sAAI",
+        authDomain: "lab5seg3102-64b36.firebaseapp.com",
+        projectId: "lab5seg3102-64b36",
+        storageBucket: "lab5seg3102-64b36.appspot.com",
+        messagingSenderId: "97182804644",
+        appId: "1:97182804644:web:3ed0caf678dd0dd1e61431",
+        measurementId: "G-XCH7EYD3SM"
+    }
+};
